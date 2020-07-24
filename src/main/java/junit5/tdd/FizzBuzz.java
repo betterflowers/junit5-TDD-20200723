e6560aa227627a659e6166ec3b4d6722f71b0581
KAROL CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENKA10@OOCL.COM>
2020-07-24 09:08:27 +0800

    [karol] feature:pass should_return_invalid_when_guess_given_answer_1234_and_input_guess_minusNumber

--- src/main/java/junit5/tdd/FizzBuzz.java
+++ src/main/java/junit5/tdd/FizzBuzz.java
@@ -3,8 +3,22 @@ package junit5.tdd;
 public class FizzBuzz {
 
     public String say(int input) {
+        if (input%3==0 && input%5==0 && input%7==0){
+            return "FizzBuzzWhizz";
+        }
+        if (input%3==0 && input%5==0){
+            return "FizzBuzz";
+        }if (input%3==0 && input%7==0){
+            return "FizzWhizz";
+        }if (input%5==0 && input%7==0){
+            return "BuzzWhizz";
+        }
         if(input%3==0){
             return "Fizz";
+        }if(input%5==0){
+            return "Buzz";
+        }if(input%7==0){
+            return "Whizz";
         }else{
             return String.valueOf(input);
         }
--- src/test/java/junit5/tdd/FizzBuzzTest.java
+++ src/test/java/junit5/tdd/FizzBuzzTest.java
@@ -15,12 +15,59 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void should_return_fizz_given_one_then_countOff(){
+    public void should_return_fizz_given_three_then_countOff(){
         FizzBuzz fizzBuzz = new FizzBuzz();
         int input = 3;
         String infoString = fizzBuzz.say(input);
         assertThat(infoString, is("Fizz"));
     }
 
+    @Test
+    public void should_return_buzz_given_five_then_countOff(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        int input = 5;
+        String infoString = fizzBuzz.say(input);
+        assertThat(infoString, is("Buzz"));
+    }
+
+    @Test
+    public void should_return_FizzBuzz_given_fifteen_then_countOff(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        int input = 15;
+        String infoString = fizzBuzz.say(input);
+        assertThat(infoString, is("FizzBuzz"));
+    }
+
+    @Test
+    public void should_return_Whizz_given_seven_then_countOff(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        int input = 7;
+        String infoString = fizzBuzz.say(input);
+        assertThat(infoString, is("Whizz"));
+    }
+
+    @Test
+    public void should_return_FizzWhizz_given_threeSeven_then_countOff(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        int input = 21;
+        String infoString = fizzBuzz.say(input);
+        assertThat(infoString, is("FizzWhizz"));
+    }
+
+    @Test
+    public void should_return_BuzzWhizz_given_fiveSeven_then_countOff(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        int input = 35;
+        String infoString = fizzBuzz.say(input);
+        assertThat(infoString, is("BuzzWhizz"));
+    }
+
+    @Test
+    public void should_return_FizzBuzzWhizz_given_threeFiveSeven_then_countOff(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        int input = 105;
+        String infoString = fizzBuzz.say(input);
+        assertThat(infoString, is("FizzBuzzWhizz"));
+    }
 
 }
